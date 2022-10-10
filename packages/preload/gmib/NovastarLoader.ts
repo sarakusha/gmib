9dbad5fa2c4a7ec5be596f36f09de7a55aaa047d
sarakusha <sarakusha@gmail.com>
2022-10-10 10:37:52 +0300

    fix: serial device polling instead of parallel

--- packages/preload/gmib/NovastarLoader.ts
+++ packages/preload/gmib/NovastarLoader.ts
@@ -1,6 +1,5 @@
 import type ScreenConfigurator from '@novastar/screen/ScreenConfigurator';
 import getCabinetPosition from '@novastar/screen/getCabinetPosition';
-
 import flatten from 'lodash/flatten';
 import pMap from 'p-map';
 
@@ -25,34 +24,43 @@ export default class NovastarLoader extends Runnable<
 
   protected async runImpl({ selectors }: NovastarOptions): Promise<CabinetInfo[]> {
     return flatten(
-      await pMap(this.controller.screens, async (screen, screenIndex) => {
-        const statusGen = this.controller.ReadHWStatus(screenIndex);
-        const fpgaVersionGen = this.controller.ReadReceivingCardFPGARemarks(screenIndex);
-        const mcuVersionGen = this.controller.ReadReceivingCardMCURemarks(screenIndex);
-        const addresses = this.controller.GetScreenAllPort(screenIndex, true);
-        const result = await pMap(addresses, async ({ SenderIndex, PortIndex, ScanIndex }) => {
-          if (this.isCanceled) return null;
-          const position = getCabinetPosition(screen, SenderIndex, PortIndex, ScanIndex);
-          if (!position) throw new Error('Unknown position');
-          const cabinetInfo: CabinetInfo = {
-            ...position,
-            status:
-              selectors.has(NovastarSelector.Voltage) || selectors.has(NovastarSelector.Temperature)
-                ? (await statusGen.next()).value
-                : undefined,
-            fpgaVersion: selectors.has(NovastarSelector.FPGA_Version)
-              ? (await fpgaVersionGen.next()).value
-              : undefined,
-            mcuVersion: selectors.has(NovastarSelector.MCU_Version)
-              ? (await mcuVersionGen.next()).value
-              : undefined,
-            screen: screenIndex,
-          };
-          this.emit('cabinet', cabinetInfo);
-          return cabinetInfo;
-        });
-        return result.filter(notEmpty);
-      }),
+      await pMap(
+        this.controller.screens,
+        async (screen, screenIndex) => {
+          const statusGen = this.controller.ReadHWStatus(screenIndex);
+          const fpgaVersionGen = this.controller.ReadReceivingCardFPGARemarks(screenIndex);
+          const mcuVersionGen = this.controller.ReadReceivingCardMCURemarks(screenIndex);
+          const addresses = this.controller.GetScreenAllPort(screenIndex, true);
+          const result = await pMap(
+            addresses,
+            async ({ SenderIndex, PortIndex, ScanIndex }) => {
+              if (this.isCanceled) return null;
+              const position = getCabinetPosition(screen, SenderIndex, PortIndex, ScanIndex);
+              if (!position) throw new Error('Unknown position');
+              const cabinetInfo: CabinetInfo = {
+                ...position,
+                status:
+                  selectors.has(NovastarSelector.Voltage) ||
+                  selectors.has(NovastarSelector.Temperature)
+                    ? (await statusGen.next()).value
+                    : undefined,
+                fpgaVersion: selectors.has(NovastarSelector.FPGA_Version)
+                  ? (await fpgaVersionGen.next()).value
+                  : undefined,
+                mcuVersion: selectors.has(NovastarSelector.MCU_Version)
+                  ? (await mcuVersionGen.next()).value
+                  : undefined,
+                screen: screenIndex,
+              };
+              this.emit('cabinet', cabinetInfo);
+              return cabinetInfo;
+            },
+            { concurrency: 1 },
+          );
+          return result.filter(notEmpty);
+        },
+        { concurrency: 1 },
+      ),
     );
   }
 }
