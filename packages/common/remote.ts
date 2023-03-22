5cfd83301c63a094629ee2f45207a1aac5285ce1
sarakusha <sarakusha@gmail.com>
2023-03-22 12:29:10 +0300

    feat: get url for the remote session

--- packages/common/remote.ts
+++ packages/common/remote.ts
@@ -2,3 +2,5 @@ const query = window && new URLSearchParams(window.location.search);
 export const port = +(query?.get('port') ?? 9001);
 export const host = query?.get('host') ?? 'localhost';
 export const isRemoteSession = host !== 'localhost';
+
+export const getUrl = (path: string): string => isRemoteSession ? `http://${host}:${port + 1}${path}` : path;
