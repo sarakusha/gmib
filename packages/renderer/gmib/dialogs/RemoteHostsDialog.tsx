05209b2c234bd1fdac70006066a87d8d5056f1f1
sarakusha <sarakusha@gmail.com>
2023-06-08 09:23:24 +0300

    fix: iput

--- packages/renderer/gmib/dialogs/RemoteHostsDialog.tsx
+++ packages/renderer/gmib/dialogs/RemoteHostsDialog.tsx
@@ -14,6 +14,8 @@ import { styled } from '@mui/material/styles';
 import { hasProps } from '@novastar/screen/common';
 import React, { useEffect, useRef, useState } from 'react';
 import type { MouseEvent } from 'react';
+import { ErrorBoundary, type FallbackRender } from 'react-error-boundary';
+import IPut from 'iput';
 
 import type { CustomHost } from '/@common/helpers';
 
@@ -21,8 +23,6 @@ import FormFieldSet from '../components/FormFieldSet';
 import { useSelector } from '../store';
 import { selectAllRemoteHosts } from '../store/selectors';
 import timeid from '../util/timeid';
-
-import IPut from 'iput';
 
 export type RemoteHostsDialogProps = {
   open?: boolean;
@@ -68,6 +68,15 @@ const Header = styled('div')(({ theme }) => ({
 }));
 
 const hasAddressPort = hasProps('address', 'port');
+
+const fallbackRender: typeof FallbackRender = ({ error }) => (
+  // Call resetErrorBoundary() to reset the error boundary and retry the render.
+
+  <div role="alert">
+    <p>Something went wrong:</p>
+    <pre style={{ color: 'red' }}>{error.message}</pre>
+  </div>
+);
 
 const RemoteHostsDialog: React.FC<RemoteHostsDialogProps> = ({
   open = false,
@@ -167,22 +176,24 @@ const RemoteHostsDialog: React.FC<RemoteHostsDialogProps> = ({
             {customHosts &&
               customHosts.map(({ address, port, id }, index) => (
                 <React.Fragment key={id}>
-                  <IPut
-                    defaultValue={address ?? '...'}
-                    onChange={makeAddressHandler(id)}
-                    css={theme => ({
-                      padding: 0,
-                      border: 'none',
-                      borderRadius: 0,
-                      '& input': {
-                        borderBottom: '1px solid rgba(0, 0, 0, 0.42)',
-                        fontFamily: theme.typography.fontFamily,
-                        fontSize: 16,
-                        padding: '6px 0',
-                        width: '4ch',
-                      },
-                    })}
-                  />
+                  <ErrorBoundary fallbackRender={fallbackRender}>
+                    <IPut
+                      defaultValue={address ?? '...'}
+                      onChange={makeAddressHandler(id)}
+                      css={theme => ({
+                        padding: 0,
+                        border: 'none',
+                        borderRadius: 0,
+                        '& input': {
+                          borderBottom: '1px solid rgba(0, 0, 0, 0.42)',
+                          fontFamily: theme.typography.fontFamily,
+                          fontSize: 16,
+                          padding: '6px 0',
+                          width: '4ch',
+                        },
+                      })}
+                    />
+                  </ErrorBoundary>
                   <TextField
                     variant="standard"
                     value={port ?? ''}
@@ -239,6 +250,145 @@ const RemoteHostsDialog: React.FC<RemoteHostsDialogProps> = ({
           Отмена
         </Button>
       </DialogActions>
+    </Dialog>
+  );
+};
+
+const RemoteHostsDialogStub: React.FC<RemoteHostsDialogProps> = ({
+  open = false,
+  onClose = () => {},
+}) => {
+  const remoteHosts = useSelector(selectAllRemoteHosts);
+  const [customHosts, setCustomHosts] = useState<CustomHostItem[]>([]);
+  const [changed, setChanged] = useState(false);
+  const saveHandler = (): void => {
+    const valid = customHosts.filter(hasAddressPort).map(({ address, port }) => ({
+      address,
+      port: +port,
+    }));
+    window.config.set('hosts', valid);
+    onClose();
+  };
+  const cancelHandler = onClose;
+  // const dispatch = useDispatch();
+  useEffect(() => {
+    const updateHosts = (hosts: CustomHost[] = []): void => {
+      setCustomHosts(
+        hosts.map<CustomHostItem>(({ address, port }) => ({
+          address,
+          port: port.toString(),
+          id: timeid(),
+        })),
+      );
+      setChanged(false);
+    };
+    window.config.get('hosts').then(updateHosts);
+  }, [open]);
+  const refLast = useRef<HTMLDivElement>(null);
+
+  function makeHandler<T>(
+    upd: (arg: T, index: number, customs: CustomHostItem[]) => void,
+  ): (id: string) => (arg: T) => void {
+    return (id: string) =>
+      (arg: T): void => {
+        const customs = [...customHosts];
+        const index = customs.findIndex(item => item.id === id);
+        if (index !== -1) {
+          upd(arg, index, customs);
+          setCustomHosts(customs);
+          setChanged(true);
+        }
+      };
+  }
+
+  const makeAddressHandler = makeHandler((address: string, index, customs) => {
+    const host = customs[index];
+    host.address = address;
+  });
+  const makePortHandler = makeHandler((e: React.ChangeEvent<HTMLInputElement>, index, customs) => {
+    const host = customs[index];
+    host.port = e.target.value;
+  });
+  const makeCloseHandler = makeHandler((e: MouseEvent, index, customs) => {
+    customs.splice(index, 1);
+  });
+  return (
+    <Dialog open={open} aria-labelledby="remote-hosts-title" maxWidth="md">
+      <DialogTitle id="remote-hosts-title">Список удаленных хостов</DialogTitle>
+      <DialogContent sx={{ display: 'flex', flexDirection: 'column' }}>
+        {/* <FieldSet legend="Найденные в сети">
+            <Remote>
+              <Header>
+                <div>Адрес</div>
+                <div>Порт</div>
+                <div>Хост</div>
+                <div>Версия</div>
+              </Header>
+              <Box
+                sx={{
+                  display: 'contents',
+                  color: 'text.disabled',
+                }}
+              >
+                {remoteHosts.map(({ name, address, port, version }) => (
+                  <React.Fragment key={name}>
+                    <div>{address}</div>
+                    <div>{port}</div>
+                    <div>{name}</div>
+                    <div>{version}</div>
+                  </React.Fragment>
+                ))}
+              </Box>
+            </Remote>
+          </FieldSet> */}
+        <FieldSet legend="Пользовательские">
+          <Remote>
+            <Header>
+              <div>Адрес</div>
+              <div>Порт</div>
+              <div />
+              <div />
+            </Header>
+            {customHosts &&
+              customHosts.map(({ address, port, id }, index) => (
+                <React.Fragment key={id}>
+                  {/* <IPut
+                      defaultValue={address ?? '...'}
+                      onChange={makeAddressHandler(id)}
+                      css={theme => ({
+                        padding: 0,
+                        border: 'none',
+                        borderRadius: 0,
+                        '& input': {
+                          borderBottom: '1px solid rgba(0, 0, 0, 0.42)',
+                          fontFamily: theme.typography.fontFamily,
+                          fontSize: 16,
+                          padding: '6px 0',
+                          width: '4ch',
+                        },
+                      })}
+                    /> */}
+                  <TextField
+                    variant="standard"
+                    value={port ?? ''}
+                    type="number"
+                    onChange={makePortHandler(id)}
+                    InputProps={portProps}
+                  />
+                  <Box
+                    alignSelf="flex-start"
+                    ref={index === customHosts.length - 1 ? refLast : undefined}
+                  >
+                    <IconButton size="small" title="Удалить" onClick={makeCloseHandler(id)}>
+                      <CloseIcon fontSize="inherit" />
+                    </IconButton>
+                  </Box>
+                  <div />
+                </React.Fragment>
+              ))}
+          </Remote>
+        </FieldSet>
+      </DialogContent>
     </Dialog>
   );
 };
