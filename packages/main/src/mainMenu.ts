83c934d1412f1ea4ff6fd99a9fb84d04116f6f87
sarakusha <sarakusha@gmail.com>
2023-12-21 15:51:58 +0300

    fix: сообщение отправлялось закрытому окну

--- packages/main/src/mainMenu.ts
+++ packages/main/src/mainMenu.ts
@@ -505,7 +505,7 @@ const blurWindows: BrowserWindow[] = [];
 app.on('browser-window-focus', (_, win) => {
   win.webContents.send('focus', true);
   const wins = blurWindows.splice(0, blurWindows.length);
-  wins.forEach(item => win !== item && item.webContents.send('focus', false));
+  wins.filter(item => win !== item && !item.isDestroyed()).forEach(item => item.webContents.send('focus', false));
   updateMenu();
 });
 
