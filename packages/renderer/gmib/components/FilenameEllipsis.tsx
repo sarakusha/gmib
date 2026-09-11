04ac20b52457a6f68810d8aac90ce5cdae84ce8c
Andrei Sarakeev <sarakusha@gmail.com>
2026-09-11 15:36:18 +0300

    fix: constrain long filenames in file selectors

--- packages/renderer/gmib/components/FilenameEllipsis.tsx
+++ packages/renderer/gmib/components/FilenameEllipsis.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Tooltip, Typography } from '@mui/material';
 import type { SxProps, Theme } from '@mui/material/styles';
 import React, { memo } from 'react';
 
@@ -13,24 +13,28 @@ const FilenameEllipsis: React.FC<Props> = ({ filename = '', sx, placeholder }) =
   const [pathPrefix, name] =
     fileIndex !== -1 ? [filename.substr(0, fileIndex), filename.slice(fileIndex)] : ['', filename];
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexWrap: 'nowrap',
-        '& > *': {
-          whiteSpace: 'nowrap',
-          overflow: 'hidden',
-          textOverflow: 'ellipsis',
-        },
-        ...sx,
-      }}
-    >
-      {filename.length === 0 && placeholder && (
-        <Typography sx={{ opacity: 0.5 }}>{placeholder}</Typography>
-      )}
-      {pathPrefix.length > 0 && <Typography sx={{ flexShrink: 10000 }}>{pathPrefix}</Typography>}
-      <Typography sx={{ flexShrink: 0.1 }}>{name}</Typography>
-    </Box>
+    <Tooltip title={filename} disableHoverListener={!filename}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'nowrap',
+          minWidth: 0,
+          '& > *': {
+            minWidth: 0,
+            whiteSpace: 'nowrap',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          },
+          ...sx,
+        }}
+      >
+        {filename.length === 0 && placeholder && (
+          <Typography sx={{ opacity: 0.5 }}>{placeholder}</Typography>
+        )}
+        {pathPrefix.length > 0 && <Typography sx={{ flexShrink: 10000 }}>{pathPrefix}</Typography>}
+        <Typography sx={{ flexShrink: 0.1 }}>{name}</Typography>
+      </Box>
+    </Tooltip>
   );
 };
 
